f3f42ed85bfe98cb6039d5fab23447396fab680d
Mazen <mazenhilalpmp@gmail.com>
2020-11-18 18:59:17 +0000

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
     if (!location) {
         return messageOne.textContent = 'Enter your location'
     }
-    fetch('http://localhost:3000/weather?address=' + encodeURIComponent(location)).then((response) => {
+    fetch('/weather?address=' + encodeURIComponent(location)).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error)
