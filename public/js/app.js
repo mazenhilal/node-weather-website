f1145895c09c825cd5863664dfc09877423463d1
Mazen <mazenhilalpmp@gmail.com>
2020-11-18 12:46:47 +0000

    Change the console

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side javascript ...')
+console.log('Client side javascript ....')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
